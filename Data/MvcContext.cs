2cb509a8ea172a0022ec29a68cfd5718ffd2d341
infinityc2 <super_star@hotmail.co.th>
2020-01-28 12:34:24 +0700

    add mvc crud :tada:

---
+++ Data/MvcContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Models;
+
+namespace WebApplication.Data
+{
+    public class MvcContext : DbContext
+    {
+        public MvcContext(DbContextOptions<MvcContext> options) : base(options)
+        {
+        }
+        public DbSet<Movie> movies { get; set; }
+    }
+}
